c4dd47798e93684241a197079ae6596f409537a5
Pratik Valvi <pratikvalvi2305@gmail.com>
2023-07-16 22:58:54 +0530

    Configured: Chakra UI in App

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+
+import { Container, Heading } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Next.js
+    <main>
+      <Container maxWidth="container.xl" padding="10px 10px 0px 10px" flexDirection="column" justifyContent="center" alignItems="center">
+        <Heading>Hello Next.js</Heading>
+      </Container>
     </main>
   )
 }
